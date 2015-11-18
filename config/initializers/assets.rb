465cee42ee32e5021ec407b9611a5f603162a1d5
Thiago Fantin <thiago_fantin@hotmail.com>
2015-11-18 17:04:38 -0200

    Rotas: Directions Multiple Points

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( jquery.js )
 Rails.application.config.assets.precompile += %w( mapsgoogle.js )
 Rails.application.config.assets.precompile += %w( routes.js )
+Rails.application.config.assets.precompile += %w( teste.js )
 Rails.application.config.assets.precompile += %w( apigm.js )
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( style.css )
